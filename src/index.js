2d07c6aee2024516907d32625410e1a78afea2fa
Ishwor kumal <ikteck99@gmail.com>
2021-01-12 13:43:25 +0545

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+require('./models/User');
+require('./models/Track');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireAuth = require('./middlewares/requireAuth');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/', authRoutes);
+
+app.use(trackRoutes);
+
+const mongoUri =
+  "mongodb+srv://admin:passwordpassword@cluster0.i7auj.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo Instance');
+})
+
+mongoose.connection.on('error', (err) => {
+console.log('Error connecting to mongo', err);
+})
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email is ${req.user.email}`);
+});
+
+app.listen(3000, () => console.log(`Listening on port no 3000`));
